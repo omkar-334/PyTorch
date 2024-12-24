acea528a7e5bab084f5e57a115f3979d0c5768e2
Omkar Kabde <omkarkabde@gmail.com>
2024-12-24 16:42:04 +0530

    sigmoid

--- activation_functions.py
+++ activation_functions.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -6,3 +7,8 @@ class ActivationFunction(nn.Module):
         super().__init__()
         self.name = self.__class__.__name__
         self.config = {"name": self.name}
+
+
+class Sigmoid(ActivationFunction):
+    def forward(self, x):
+        return 1 / (1 + torch.exp(-x))
