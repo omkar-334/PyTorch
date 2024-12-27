caaf2ec1a1fc29f04d6b6bdcbf2f1085350100e2
Omkar Kabde <omkarkabde@gmail.com>
2024-12-27 19:20:05 +0530

    activation functions

--- activation_functions.py
+++ activation_functions.py
@@ -16,27 +16,39 @@ class Sigmoid(ActivationFunction):
 
 class Tanh(ActivationFunction):
     def forward(self, x):
-        return (torch.exp(x) - torch.exp(-x)) / (torch.exp(x) + torch.exp(-x))
+        x_exp, neg_x_exp = torch.exp(x), torch.exp(-x)
+        return (x_exp - neg_x_exp) / (x_exp + neg_x_exp)
 
 
 class ReLU(ActivationFunction):
     def forward(self, x):
-        return max(0, x)
+        return x * (x > 0).float()
 
 
 class LeakyReLU(ActivationFunction):
+    def __init__(self, alpha=0.1):
+        super().__init__()
+        self.config["alpha"] = alpha
+
     def forward(self, x):
-        return max(0.1 * x, x)
+        return torch.where(x > 0, x, self.config["alpha"] * x)
 
 
 class ELU(ActivationFunction):
     def forward(self, x):
-        if x < 0:
-            return torch.exp(x) - 1
-        else:
-            return x
+        return torch.where(x > 0, x, torch.exp(x) - 1)
 
 
 class Swish(ActivationFunction):
     def forward(self, x):
         return x * torch.sigmoid(x)
+
+
+act_fn_by_name = {
+    "sigmoid": Sigmoid,
+    "tanh": Tanh,
+    "relu": ReLU,
+    "leakyrelu": LeakyReLU,
+    "elu": ELU,
+    "swish": Swish,
+}
